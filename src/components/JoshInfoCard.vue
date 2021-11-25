ff98c96f1f3bf65ca902a46da1a2bb29dc0a65c3
sinapples <Josh.lopez.is@gmail.com>
2021-11-25 15:48:24 -0600

    poc

---
+++ src/components/JoshInfoCard.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-card color="secondary px-0" dark tile>
+    <v-sheet color="primary  ">
+      <v-card-title class="justify-center text-h7  white--text">
+        <div class="justify-center">
+          <span class="justify-center line mr-2">Hi I'm Josh! </span>
+          <span class="justify-center line">{{ appTitle }} </span>
+        </div>
+      </v-card-title>
+    </v-sheet>
+
+    <v-card-text class=" body-1 text-center px-0 mb-0">
+      <v-container class="px-0">
+        <v-row align="center" justify="center" no-gutters>
+          <v-spacer />
+
+          <v-col cols="auto">
+            <v-list-item-avatar class="ma-3" size="125" right rounded>
+              <v-img
+                src="https://firebasestorage.googleapis.com/v0/b/super-effective-eea44.appspot.com/o/photo-squashed.JPG?alt=media&token=b55d8a0c-bacf-40df-901c-c084a183ced0"
+              ></v-img>
+            </v-list-item-avatar>
+          </v-col>
+          <v-col>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-start text-h5 mb-2">
+                  Nice to meet you!
+                </v-list-item-title>
+                <div class=" text-start  body-1">
+                  What's your preferred message app?
+                </div>
+                <div class=" text-start  body-1">
+                  You can tap on the buttons below to add me.
+                </div>
+              </v-list-item-content>
+            </v-list-item>
+          </v-col>
+
+          <v-spacer />
+        </v-row>
+      </v-container>
+    </v-card-text>
+    <!-- buttons -->
+
+    <v-card-actions>
+      <v-row dense justify="center">
+        <v-col cols="12">
+          <LinkButton
+            msg-app="discord"
+            icon="mdi-discord"
+            color="#5865F2"
+            user-name="sinap#3816"
+          />
+        </v-col>
+        <v-col cols="12">
+          <LinkButton
+            msg-app="Telegram"
+            icon="mdi-send"
+            color="#1B9EDE"
+            user-name="sinapples"
+            link="https://t.me/sinapples"
+          />
+        </v-col>
+        <v-col cols="12">
+          <LinkButton
+            msg-app="Facebook Messenger"
+            icon="mdi-facebook-messenger"
+            color="#5C46B8"
+            user-name="sinapples"
+            link="m.me/sinapples"
+          />
+        </v-col>
+        <v-col cols="12">
+          <LinkButton
+            msg-app="Venmo"
+            icon="mdi-credit-card-outline"
+            color="#3396CD"
+            user-name="sinapples"
+            link="www.venmo.com/"
+          />
+        </v-col>
+        <v-col cols="12">
+          <LinkButton
+            msg-app="Instagram"
+            icon="mdi-instagram"
+            color="#2174FF"
+            user-name="sinapples"
+            link="https://www.instagram.com/sinapples"
+          />
+        </v-col>
+      </v-row>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+import LinkButton from './LinkButton.vue'
+
+export default {
+  name: 'JoshInfoCard',
+  components: { LinkButton },
+
+  data() {
+    return {
+      testingCode: '1234'
+    }
+  },
+  methods: {}
+}
+</script>
---
+++ src/components/LinkButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-btn dark dense block :color="color" :href="link">
+        <v-icon class="pr-2">{{ icon }}</v-icon> {{ msgApp }}
+      </v-btn>
+      <!-- <v-btn dark dense :color="color" :href="link">
+        <v-icon class="pr-2">{{ icon }}</v-icon> {{ msgApp }}
+      </v-btn>
+      {{ userName }}
+      <v-btn @click.stop.prevent="copyUsername()"> Copy</v-btn>
+      <input id="testing-code" type="hidden" :value="userName" /> -->
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msgApp: String,
+    userName: String,
+    color: String,
+    link: String,
+    icon: String
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    copyUsername() {
+      const usernameToCopy = document.querySelector('#testing-code')
+      usernameToCopy.setAttribute('type', 'text') // 不是 hidden 才能複製
+      usernameToCopy.select()
+
+      try {
+        const successful = document.execCommand('copy')
+        const msg = successful ? 'successful' : 'unsuccessful'
+        console.log(msg)
+      } catch (err) {
+        alert('Oops, unable to copy')
+      }
+
+      /* unselect the range */
+      usernameToCopy.setAttribute('type', 'hidden')
+      window.getSelection().removeAllRanges()
+    }
+  }
+}
+</script>
