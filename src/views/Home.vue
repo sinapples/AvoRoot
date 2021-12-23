86cd28bb179dd1cb4c5b584d37aab16a175791d1
sinapples <Josh.lopez.is@gmail.com>
2021-12-22 23:43:07 -0600

    created sns bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,8 +18,10 @@
           :user-name="item.userName"
           :link="item.link"
         />
+        <!-- </v-slide-item> -->
       </v-col>
     </v-row>
+
     <v-card-title>
       Payment Apps
     </v-card-title>
@@ -37,6 +39,24 @@
       </v-col>
     </v-row>
 
+    <v-card-title>
+      Games
+    </v-card-title>
+
+    <v-row justify="space-around" align-content="center" dense>
+      <v-col v-for="item in games" :key="item" cols="11">
+        <AppBarCard
+          :msg-app="item.msgApp"
+          :icon="item.icon"
+          :color="item.color"
+          :glass-color="glassColor"
+          :user-name="item.userName"
+          :link="item.link"
+          :friend-code="item.friendCode"
+        />
+      </v-col>
+    </v-row>
+
     <v-container> </v-container>
   </div>
 </template>
@@ -44,16 +64,19 @@
 <script>
 import UserCard from '../components/UserCard'
 import AppCard from '../components/AppCard'
+import AppBarCard from '../components/AppBarCard'
 
 export default {
   name: 'Home',
 
   components: {
     UserCard,
-    AppCard
+    AppCard,
+    AppBarCard
   },
   data() {
     return {
+      model: null,
       apps: [
         {
           msgApp: 'Discord',
@@ -86,6 +109,14 @@ export default {
 
           userName: 'sinapples',
           link: 'https://www.instagram.com/sinapples'
+        },
+        {
+          msgApp: 'Snapchat*',
+          icon: 'mdi-snapchat',
+          color: '#E1306C',
+
+          userName: 'sinapples',
+          link: 'https://www.instagram.com/sinapples'
         }
       ],
       payments: [
@@ -95,6 +126,60 @@ export default {
           color: '#3396CD',
 
           userName: 'sinapples',
+          link: 'https://venmo.com/code?user_id=2003272562900992908'
+        },
+        {
+          msgApp: 'Paypal',
+          icon: 'mdi-credit-card-outline',
+          color: '#3396CD',
+
+          userName: 'sinapples',
+          link: 'https://www.paypal.me/sinapples?locale.x=en_US'
+        },
+        {
+          msgApp: 'Buy Me Boba',
+          icon: 'mdi-tea',
+          color: '#3396CD',
+
+          userName: 'sinapples',
+          link: 'https://www.buymeacoffee.com/sinapples'
+        }
+      ],
+      games: [
+        {
+          msgApp: 'Pokemon Go',
+          icon: 'mdi-pokemon-go',
+          color: '#3396CD',
+
+          userName: 'Sinapples',
+          friendCode: '323446415079',
+          link: 'https://venmo.com/code?user_id=2003272562900992908'
+        },
+        {
+          msgApp: 'Switch',
+          icon: 'mdi-nintendo-switch',
+          color: '#3396CD',
+
+          userName: 'sinapples',
+          friendCode: 'SW-8004-7191-8923',
+          link: 'https://venmo.com/code?user_id=2003272562900992908'
+        },
+        {
+          msgApp: 'PSN',
+          icon: 'mdi-sony-playstation',
+          color: '#3396CD',
+
+          userName: 'sinapples',
+
+          link: 'https://venmo.com/code?user_id=2003272562900992908'
+        },
+        {
+          msgApp: 'Oculus',
+          icon: 'mdi-virtual-reality',
+          color: '#3396CD',
+
+          userName: 'sinapples',
+
           link: 'https://venmo.com/code?user_id=2003272562900992908'
         }
       ]
